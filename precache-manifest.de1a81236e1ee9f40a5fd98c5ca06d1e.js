657a4d39613da1b7d21ff9ab988d189d31c38763
Ritik Soni <34769340+ritik07@users.noreply.github.com>
2020-08-09 20:20:09 +0530

    Updates

---
+++ precache-manifest.de1a81236e1ee9f40a5fd98c5ca06d1e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7660229267e25c7b978bbc54d1440e57",
+    "url": "/solutions/index.html"
+  },
+  {
+    "revision": "55e10e159ed12b802568",
+    "url": "/solutions/static/css/2.cc67e0c4.chunk.css"
+  },
+  {
+    "revision": "8837cb07d897b69d11b9",
+    "url": "/solutions/static/css/main.303847df.chunk.css"
+  },
+  {
+    "revision": "55e10e159ed12b802568",
+    "url": "/solutions/static/js/2.f6b6535f.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/solutions/static/js/2.f6b6535f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8837cb07d897b69d11b9",
+    "url": "/solutions/static/js/main.cf77ba9d.chunk.js"
+  },
+  {
+    "revision": "0c3dfb1ff182e3363596",
+    "url": "/solutions/static/js/runtime-main.220aeffc.js"
+  },
+  {
+    "revision": "32b67c728ad8b691637e6832b8b11cef",
+    "url": "/solutions/static/media/Img2.32b67c72.jpg"
+  },
+  {
+    "revision": "df92c81182058492697117d089016820",
+    "url": "/solutions/static/media/blc.df92c811.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/solutions/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/solutions/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/solutions/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/solutions/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/solutions/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fd3a054f1abfa8be8cc81359718f3ca6",
+    "url": "/solutions/static/media/pizza.fd3a054f.png"
+  },
+  {
+    "revision": "bed09d9264b4e83f080af8e9e0d0fde8",
+    "url": "/solutions/static/media/playvideo.bed09d92.jpg"
+  },
+  {
+    "revision": "dfb2ea3c2ab3a8dafafe2226ac6fb008",
+    "url": "/solutions/static/media/tlc.dfb2ea3c.png"
+  }
+]);
